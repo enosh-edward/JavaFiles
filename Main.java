88ce5385d404fbba4cacbeaea98b05ee2efd38d5
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-05-16 16:12:37 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Main{
+
+    public static void main(String args[]){
+        float sample=(float) 10.3;
+        String sam=String.valueOf(sample);
+        System.out.println("enosh".replace('e',' '));
+    }
+}
