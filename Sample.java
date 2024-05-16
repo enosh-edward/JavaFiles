197a13d5c99d98240c2d97aef5642a94cd35b850
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-05-16 16:18:07 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Sample {
+   private int x;
+   private int y;
+
+   public Sample(int var1, int var2) {
+      this.x = var1;
+      this.y = var2;
+   }
+}
