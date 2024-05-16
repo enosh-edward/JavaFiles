3437e69431f8507f8daf3e4f597a95ddf532b217
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-05-16 16:16:05 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class Rectangle extends Sample {
+   private int width;
+   private int height;
+
+   public Rectangle(int var1, int var2) {
+      this(var1, var2, 0, 0);
+   }
+
+   public Rectangle(int var1, int var2, int var3, int var4) {
+      super(var1, var2);
+      this.width = var3;
+      this.height = var4;
+   }
+}
