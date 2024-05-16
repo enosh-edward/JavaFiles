fa4f9c29cf782a27981641911f54ed1b4390fb3c
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-05-16 16:11:00 +0530

    Ford.java

---
+++ Ford.java
@@ -0,0 +1,14 @@
+public class Ford extends Car{
+    public Ford(){
+        super();
+    }
+    public void startEngine(){
+        super.startEngine();
+    }
+    public void accelerate(){
+        super.accelerate();
+    }
+    public void brake(){
+        super.brake();
+    }
+}
