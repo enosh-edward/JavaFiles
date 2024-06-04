a1c9a5b165900021d1a041d43e97a13ef9072fba
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-06-04 16:00:45 +0530

    Create Dog.java

---
+++ Abstraction/Dog.java
@@ -0,0 +1,22 @@
+public class Dog extends Animal {
+    
+    public Dog(String type,String size,double weight){
+        super(type,size,weight);
+    }
+    @Override
+    public void move(String speed){
+        if(speed.equals("slow")){
+            System.out.println(getExplicitType() + " Walking! ");
+        }else{
+            System.out.println(getExplicitType() +" Running");
+        }
+    }
+    @Override
+    public void makeNoise(){
+        if(type=="Wolf"){
+            System.out.println("Howling! ");
+        }else{
+            System.out.println("Woof! ");
+        }
+    }
+}
