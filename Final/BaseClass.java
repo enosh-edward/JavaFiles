3fdb208b429997ef5e0af5551433813c19c60f72
enosh-edward <84087497+enosh-edward@users.noreply.github.com>
2024-08-08 19:22:14 +0530

    Create BaseClass.java

---
+++ Final/BaseClass.java
@@ -0,0 +1,28 @@
+public class BaseClass{
+    public void recommendedMethod(){
+        System.out.println("[BaseClass.recommendedMethod]: Best Way to Do it");
+        optionalMethod();
+        mandatoryMethod();
+    }
+    protected void optionalMethod(){
+        System.out.println("[BaseClass.optionalMethod]: customize Optional Method");
+    }
+
+    private void mandatoryMethod(){
+        System.out.println("[BaseClass.mandatoryMethod]: NON_NEGOTIABLE");
+    }
+
+    public static void recommendedStatic(){
+        System.out.println("[BaseClass.recommendedStatic] BEST Way to do it");
+        optionalStatic();
+        mandatoryStatic();
+    }
+
+    protected static void optionalStatic(){
+        System.out.println("[BaseClass.optionalStatic]: Optional");
+    }
+
+    private static void mandatoryStatic(){
+        System.out.println("[BaseClass.mandatoryStatic] : MANDATORY");
+    }
+}
